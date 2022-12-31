f94c8fabf72be79ae866558412cf1cf3e602cb36
kkwon <wonkimkyo@gmail.com>
2022-12-31 11:40:25 +0900

    DOM 이용한 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+    pwd = document.querySelector("#pwd"),
+    loginBtn = document.querySelector("button");
+
+    loginBtn.addEventListener("click", login);
+
+    function login() {
+        const req = {
+            id: id.value,
+            pwd: pwd.value,
+        };
+        
+    }
